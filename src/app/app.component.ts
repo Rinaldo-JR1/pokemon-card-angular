2735bb840f60f20775d6021227d74a9b2f93e4a0
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2022-12-01 14:56:37 -0300

    Projeto dinamizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Aula de services</h1>
-  <app-card></app-card>
-  <router-outlet></router-outlet>`,
+   <app-card></app-card>
+    <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  name: string = 'Bulbasaur';
+  atributes: string[] = ['Grass', 'rock'];
 }
