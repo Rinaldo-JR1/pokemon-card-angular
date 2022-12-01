f5ab7d6dd27d751f04e1d2574be78a201da87a5f
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2022-12-01 17:07:26 -0300

    Finalizadas todas as integrações com a API

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { PokemonData } from './../../models/pokemonData';
+import { PokemonServiceService } from './../../services/pokemon-service.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
-  name: string = 'Bulbasaur';
+export class CardComponent implements OnInit {
+  pokemon: PokemonData;
+
   atributes: string[] = ['Grass', 'rock'];
+
+  constructor(private service: PokemonServiceService) {
+    this.pokemon = {
+      name: '',
+      id: 0,
+      sprites: { front_default: '' },
+      types: [{ slot: 0, type: { name: '', url: '' } }],
+    };
+    this.getPokemon('pikachu');
+  }
+
+  ngOnInit(): void {}
+
+  getPokemon(searchPokemon: string) {
+    this.service.getPokemon(searchPokemon.toLocaleLowerCase()).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types,
+        };
+      },
+      error: (err) => {
+        console.log('Not found');
+      },
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment ={
+  prduction: false,
+  pokeApi:'https://pokeapi.co/api/v2/pokemon/'
+}
